fe301c9788d2e1f61d8a86b75a7667e28ecc560b
mai <168496714@qq.com>
2018-03-02 16:38:27 +0800

    Updated demo

--- src/screen/demo/index.js
+++ src/screen/demo/index.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { inject, observer } from 'mobx-react'
 
 import { Container } from './component'
+import { observable } from 'mobx'
 
 type Props = {
   count: number,
@@ -29,12 +30,21 @@ const Button = styled.button`
 }))
 @observer
 export default class Demo extends Component<Props> {
+  @observable testCount = 0
   render() {
     return (
       <Container>
         <Button onClick={this.props.increase}>+</Button>
         <Button onClick={this.props.decrease}>-</Button>
+        <Button
+          onClick={() => {
+            this.testCount++
+          }}
+        >
+          testCount +
+        </Button>
         <div>{this.props.count}</div>
+        <div>{this.testCount}</div>
         <TestElement />
       </Container>
     )
